d3e70b8c25ea915b43c84bb016cb04bf2fb240af
Sergiy85 <zubets85@gmail.com>
2018-03-10 01:27:01 +0200

    change Brandi

--- my_project_gulp/app/js/script.custom.js
+++ my_project_gulp/app/js/script.custom.js
@@ -43,5 +43,27 @@ $(function () {
             isScroll();
         })
     })();
+
+    (function responsiveNav () {
+        let btn = $('#menu-btn');
+        let nav = $('nav');
+        let overlay = $('.overlay');
+
+        btn.on('click', function () {
+            nav.addClass('nav--active');
+            overlay.addClass('overlay--active');
+            $('body').addClass('body-fixed');
+
+            $(this).attr('disabled', true);
+        });
+
+        overlay.on('click', function () {
+            nav.removeClass('nav--active');
+            overlay.removeClass('overlay--active');
+            $('body').removeClass('body-fixed');
+
+            btn.removeAttr('disabled');
+        })
+    })();
     
 })
